2002172cd7d406c7d666d4ea8be3904dc63c7610
CA <choirulsubwofer12@gmail.com>
2019-08-21 20:57:57 +0700

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'sql102.epizy.com',
+  'epiz_24359785',
+  'mdZEzrATvzR6l',
+  'epiz_24359785_cobakoneksi'
+) or die(mysqli_erro($mysqli));
+
+?>
